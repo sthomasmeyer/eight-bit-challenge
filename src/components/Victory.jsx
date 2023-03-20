dca219690766c6aca0112a0510661e73c597e30e
Seth Thomasmeyer <sthomasmeyer@gmail.com>
2023-03-19 21:38:12 -0500

    adjusted base-level margin and padding (+) edited victory message

--- src/components/Victory.jsx
+++ src/components/Victory.jsx
@@ -8,7 +8,7 @@ const Victory = () => {
         <h1>Victory</h1>
       </div>
       <div className='Victory-message'>
-        <p>Congratulations anon, you're an 8-bit master.</p>
+        <p>Congratulations anon, you've completed the 8-bit challenge.</p>
       </div>
     </div>
   );
